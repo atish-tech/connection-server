31fe5adb342ec2f9f7e625d03634b3404cd94846
aatishthakur <atishthakur@truedax.io>
2024-04-24 22:14:47 +0530

    change app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Connection Server",
-  description: "Server",
+  description: "Server", 
 }; 
 
 export default function RootLayout({
